4abedc64411576b9435242a00791daa56759deeb
Steven Ng <steven.ng@braintapper.com>
2020-03-01 11:52:26 -0500

    App page initial

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script lang="coffeescript">
+
+  `export let environment`
+
+
+
+
+  import { collection, databaseLoaded } from './helpers/stores.js'
+
+
+
+  import Router from 'svelte-spa-router'
+  import {push,pop, location, link, replace} from 'svelte-spa-router'
+
+
+  import Index from './pages/Index.svelte'
+
+  import Sugar from "sugar"
+  Sugar.extend()
+
+
+
+
+
+  routes =
+    '/': Index
+
+  appName = "Svelte-Components"
+
+
+
+
+
+</script>
+<style>
+</style>
+
+<template lang="pug">
+  svelte:head
+    title { appName }
+    //link(rel="stylesheet" href="./css/fullcalendar/core/main.css" lang="css" media=screen)
+    //link(rel="stylesheet" href="./css/fullcalendar/daygrid/main.css" lang="css" media=screen)
+    //link(rel="stylesheet" href="./css/fullcalendar/timegrid/main.css" lang="css" media=screen)
+    //link(rel="stylesheet" href="./css/fullcalendar/list/main.css" lang="css" media=screen)
+
+  Router('{routes}')
+
+</template>
