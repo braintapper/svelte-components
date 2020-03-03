b529a3c70b2a83187e2d38426a1fc821885ca633
Steven Ng <steven.ng@braintapper.com>
2020-03-03 14:06:37 -0500

    Global css fine tune

--- src/pages/form/Common.svelte
+++ src/pages/form/Common.svelte
@@ -12,14 +12,35 @@
 
   <h3>Text Inputs</h3>
   <div>
+  <input type="text"/>
+  <input type="text" disabled value="disabled"/>
+  <input type="text" required/>
+  <input type="text" invalid={true}/>
   </div>
   <h3>Textareas</h3>
   <div>
   <h4>Basic</h4>
-  <textarea/>
+  <textarea required/>
   <h4>Auto Expanding</h4>
   <Autogrow/>
   </div>
-
+  <h3>Radio Buttons</h3>
+  <div>
+  <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label><br>
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Female</label><br>
+  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">Other</label>
+  </div>
+  <h3>Checkboxes</h3>
+  <div>
+  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+  <label for="vehicle1"> I have a bike</label><br>
+  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+  <label for="vehicle2"> I have a car</label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3"> I have a boat</label><br>
+  </div>
 
 </Section>
