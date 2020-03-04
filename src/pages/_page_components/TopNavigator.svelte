d01086e7ac9d2577758671c1e9b1926aca131840
Steven Ng <steven.ng@braintapper.com>
2020-03-03 19:01:00 -0500

    Cleanup

--- src/pages/_page_components/TopNavigator.svelte
+++ src/pages/_page_components/TopNavigator.svelte
@@ -1,15 +1,19 @@
 <script lang="coffeescript">
   `export let pages = []`
   `export let style = undefined`
+  `export let theme = undefined`
+  import {location} from 'svelte-spa-router'
 </script>
 <style lang="sass">
+  [active="true"]
+    background-color: var(--blue)
   div > div > a
     padding: 0px 12px
 </style>
 
 <div layout="row" {style}>
   {#each pages as page}
-  <div flex="initial" >
+  <div flex="initial" active={page.url == $location}>
     <a href="#{page.url}">{page.text}</a>
   </div>
   {/each}
